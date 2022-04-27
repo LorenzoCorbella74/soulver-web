e3c4719294292c10a7cc1184462ab9cc3a18c7db
Lorenzo Corbella <ing.lorenzo.corbella@gmail.com>
2022-04-27 10:44:20 +0200

    Update app.js
    No more api call to exchangeratesapi, just a mock

--- app.js
+++ app.js
@@ -478,16 +478,31 @@ function formatDate (date) {
 }
 
 function getCurrencies () {
-    // fetch actual currency conversion rates
-    return fetch('https://api.exchangeratesapi.io/latest')
+    const data = {
+        "success": true,
+        "timestamp": 1519296206,
+        "base": "EUR",
+        "date": "2021-03-17",
+        "rates": {
+            "AUD": 1.566015,
+            "CAD": 1.560132,
+            "CHF": 1.154727,
+            "CNY": 7.827874,
+            "GBP": 0.882047,
+            "JPY": 132.360679,
+            "USD": 1.23396,
+        }
+    };
+    /* return fetch('https://api.exchangeratesapi.io/latest')
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
+        */
             console.log('Currencies:', data);
             localStorage.setItem(`currencies-${data.date}`, JSON.stringify(data))
             return createUnit(data);
-        });
+       /*  }); */
 }
 
 function createUnit (data) {
